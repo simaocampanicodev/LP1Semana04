a112a272d388d0dae5674d94a86563ede0365c2a
txger. <simaocampanico.dev@gmail.com>
2025-03-12 14:59:14 +0000

    Solving the first exercise in the project BetterDecorador.

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string text = args[0];
+            char decorator;
+            int repeat;
+
+            if (!char.TryParse(args[1], out decorator))
+            {
+                return;
+            }
+
+            if (!int.TryParse(args[2], out repeat) || repeat < 0)
+            {
+                return;
+            }
+            Console.WriteLine(Decor(text, decorator, repeat));
+        }
+
+        private static string Decor(string s, char dec, int count)
+        {
+            string decoration = new string(dec, count);
+            return $"{decoration} {s} {decoration}";
+        }
+    }
+}
