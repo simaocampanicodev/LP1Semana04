07975822c4d649ef12f1c6aebf61b2721c5b1971
txger. <simaocampanico.dev@gmail.com>
2025-03-13 14:55:06 +0000

    Adding new summarys and editing the ones that were already made.

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -2,13 +2,23 @@
 
 namespace BetterDecorador
 {
+    /// <summary>
+    /// This program takes three command-line arguments: string, char and int.
+    /// It decorates the given string surrounding it with the given char and
+    /// with the amount of times given with the int.
+    /// </summary>
     public class Program
     {
         /// <summary>
-        /// Identifies the first, second and third arguments
-        /// and calls them with the method Decor
+        /// Parse command-line arguments and then calls the Decor
+        /// method to format the string.
         /// </summary>
-        /// <param name="args"></param>
+        /// <param name="args">
+        /// Array of strings containing:
+        /// args[0] => Text in the middle of the decorator
+        /// args[1] => The decorator
+        /// args[2] => Many times the decorator is repeated
+        // </param>
         private static void Main(string[] args)
         {
             string text = args[0];
@@ -27,14 +37,12 @@ namespace BetterDecorador
             Console.WriteLine(Decor(text, decorator, repeat));
         }
         /// <summary>
-        /// Returns a string representation of the decorated string
-        /// with the corresponding text, decoration and the amount of repeat
-        /// decorations
+        /// Decorates a string by surrounding it with a repeating character.
         /// </summary>
-        /// <param name="s"></param>
-        /// <param name="dec"></param>
-        /// <param name="count"></param>
-        /// <returns></returns>
+        /// <param name="s">Text in the middle of the decoration</param>
+        /// <param name="dec">The character used for the decoration</param>
+        /// <param name="count">Number of times the decoration repeat</param>
+        /// <returns>A formatted string with the decoration applied</returns>
 
         private static string Decor(string s, char dec, int count)
         {
