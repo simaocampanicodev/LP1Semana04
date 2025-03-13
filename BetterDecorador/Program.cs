fc496fc7e6b7810d52cafb42cb05cf5047d98831
txger. <simaocampanico.dev@gmail.com>
2025-03-13 15:03:38 +0000

    Adding a new method when the user doesn't specify the args and giving it a summary.

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -3,24 +3,29 @@
 namespace BetterDecorador
 {
     /// <summary>
-    /// This program takes three command-line arguments: string, char and int.
-    /// It decorates the given string surrounding it with the given char and
-    /// with the amount of times given with the int.
+    /// This program decorates a given string by surrounding it with a repeated character.
+    /// If no arguments are provided, it uses a default message.
     /// </summary>
     public class Program
     {
         /// <summary>
-        /// Parse command-line arguments and then calls the Decor
-        /// method to format the string.
+        /// Entry point of the program. Parses command-line arguments and calls the appropriate Decor method.
         /// </summary>
         /// <param name="args">
-        /// Array of strings containing:
-        /// args[0] => Text in the middle of the decorator
-        /// args[1] => The decorator
-        /// args[2] => Many times the decorator is repeated
-        // </param>
+        /// An array of strings containing:
+        /// args[0] - The text to be decorated.
+        /// args[1] - A single character used as the decorator.
+        /// args[2] - An integer specifying how many times the decorator character should be repeated.
+        /// If no arguments are provided, a default message is used.
+        /// </param>
         private static void Main(string[] args)
         {
+            if (args.Length < 3)
+            {
+                Console.WriteLine(Decor());
+                return;
+            }
+
             string text = args[0];
             char decorator;
             int repeat;
@@ -34,20 +39,30 @@ namespace BetterDecorador
             {
                 return;
             }
+
             Console.WriteLine(Decor(text, decorator, repeat));
         }
-        /// <summary>
-        /// Decorates a string by surrounding it with a repeating character.
-        /// </summary>
-        /// <param name="s">Text in the middle of the decoration</param>
-        /// <param name="dec">The character used for the decoration</param>
-        /// <param name="count">Number of times the decoration repeat</param>
-        /// <returns>A formatted string with the decoration applied</returns>
 
+        /// <summary>
+        /// Decorates a given string by surrounding it with a repeated character.
+        /// </summary>
+        /// <param name="s">The text to be decorated.</param>
+        /// <param name="dec">The character used for decoration.</param>
+        /// <param name="count">The number of times the decoration character should be repeated.</param>
+        /// <returns>A formatted string with the decoration applied.</returns>
         private static string Decor(string s, char dec, int count)
         {
             string decoration = new string(dec, count);
             return $"{decoration} {s} {decoration}";
+        }
+
+        /// <summary>
+        /// Returns a default decorated message when no arguments are provided.
+        /// </summary>
+        /// <returns>A default formatted string.</returns>
+        private static string Decor()
+        {
+            return Decor("User did not specify args!", '=', 3);
         }
     }
 }
